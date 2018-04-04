a4601dddc361da17633f902e858a831e21a72dab
Tanner Netterville <tannern@gmail.com>
2018-04-04 10:06:29 -0500

    Add License (#1)
    * Create license.txt
    
    * add license

--- jquery.colorpicker.js
+++ jquery.colorpicker.js
@@ -1,3 +1,13 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <tannern@gmail.com> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.  Tanner Netterville
+ * ----------------------------------------------------------------------------
+ */
+
+
 function rgbToHsv(r, g, b){
     r = r/255, g = g/255, b = b/255;
     var max = Math.max(r, g, b), min = Math.min(r, g, b);
